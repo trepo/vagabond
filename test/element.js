967bc1a5d671c61e23fe39f3529832cf4d6d7510
John Clark <socrates37@gmail.com>
2015-06-17 11:48:02 -0600

    more work on general layout

--- test/element.js
+++ test/element.js
@@ -7,10 +7,33 @@ describe('Element', function() {
   describe('Constructor', function() {
 
     it('Should Initialize', function() {
-      let element = new Element('1234', 'label');
+      let element = new Element(null, '1234', 'label');
       expect(element.id).to.equal('1234');
       expect(element.label).to.equal('label');
-      expect(element.properties).to.deep.equal({});
+      //expect(element.properties).to.deep.equal({});
+    });
+
+  });
+
+  describe('Getters & Setters', function() {
+
+    it('Should Error on setting id', function() {
+      let element = new Element(null, '1234', 'label');
+      expect(() => element.id = 'foo').to.throw(Error);
+    });
+
+    it('Should Error on setting label', function() {
+      let element = new Element(null, '1234', 'label');
+      expect(() => element.label = 'foo').to.throw(Error);
+    });
+
+  });
+
+  describe('Properties', function() {
+
+    it('Should add property', function() {
+      let element = new Element(null, '1234', 'label');
+      element.properties.foo = 'bar';
     });
 
   });
