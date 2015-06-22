c552b0c8cbb4a75ce3d749feabd36ca89c3e1f40
John Clark <socrates37@gmail.com>
2015-06-22 10:39:19 -0600

    tested Edge

--- test/Edge.js
+++ test/Edge.js
@@ -1,5 +1,7 @@
-import Edge from '../lib/Node.js';
+import Edge from '../lib/Edge.js';
+import Node from '../lib/Node.js';
 import Graph from '../lib/Graph.js';
+import Direction from '../lib/Direction.js';
 
 let expect = require('chai').expect;
 let levelup = require('levelup');
@@ -15,27 +17,97 @@ beforeEach(() => {
   graph = new Graph(db);
 });
 
-describe('Node', function() {
+describe('Edge', () => {
 
-  describe('Constructor', function() {
+  describe('Constructor', () => {
 
-    it('Should Initialize');
-
-  });
-
-  describe('getNode', function() {
-
-    it('Should get in node');
-
-    it('Should get out node');
-
-    it('Should Error on invalid direction');
+    it('Should Initialize', () => {
+      let node1 = new Node(graph, 'node1', 'label');
+      let node2 = new Node(graph, 'node2', 'label');
+      let edge = new Edge(graph, '1234', 'label', node1, node2);
+      expect(edge.id).to.equal('1234');
+      expect(edge.label).to.equal('label');
+    });
 
   });
 
-  describe('Persistence', function() {
+  describe('getNode', () => {
 
-    it('Should Persist on a property change');
+    it('Should get in node', (done) => {
+      let node1 = new Node(graph, 'node1', 'label');
+      let node2 = new Node(graph, 'node2', 'label');
+      let edge = new Edge(graph, '1234', 'label', node1, node2);
+
+      edge.getNode(Direction.IN)
+        .then(node => {
+          expect(node.id).to.equal('node2');
+          done();
+        })
+        .catch(error => done(error));
+
+    });
+
+    it('Should get out node', (done) => {
+      let node1 = new Node(graph, 'node1', 'label');
+      let node2 = new Node(graph, 'node2', 'label');
+      let edge = new Edge(graph, '1234', 'label', node1, node2);
+
+      edge.getNode(Direction.OUT)
+        .then(node => {
+          expect(node.id).to.equal('node1');
+          done();
+        })
+        .catch(error => done(error));
+
+    });
+
+    it('Should Error on invalid direction', (done) => {
+      let node1 = new Node(graph, 'node1', 'label');
+      let node2 = new Node(graph, 'node2', 'label');
+      let edge = new Edge(graph, '1234', 'label', node1, node2);
+
+      try {
+        edge.getNode('BOGUS');
+      } catch (error) {
+        expect(error).to.be.instanceof(Error);
+        return done();
+      }
+
+      done(new Error('Should have thrown error'));
+
+    });
+
+  });
+
+  describe('Persistence', () => {
+
+    it('Should Persist on a property change', (done) => {
+      let node1 = new Node(graph, 'node1', 'label');
+      let node2 = new Node(graph, 'node2', 'label');
+      let edge = new Edge(graph, '1234', 'label', node1, node2);
+
+      Promise.all([
+          node1._persist(),
+          node2._persist(),
+          edge._persist()
+        ])
+        .then(ignored => edge.setProperty('foo', 'bar'))
+        .then(edge => {
+          db.get('edge:1234', (error, value) => {
+            expect(error).to.be.null;
+            expect(value).to.deep.equal({
+              id: '1234',
+              label: 'label',
+              from: 'node1',
+              to: 'node2',
+              properties: {foo: 'bar'}
+            });
+            done();
+          })
+        })
+        .catch(error => done(error));
+
+    });
 
   });
 
